2f476c61b612827a6864fdd61145a0f2cd082f04
Dung Ly <ndungly@gmail.com>
2019-03-08 17:03:53 +0100

    Testlink V1

---
+++ device_Desktop/quiz_merkbeleving_chapter_1_feedback_1.js
@@ -0,0 +1,68 @@
+if (window.VarCurrentView) VarCurrentView.set('Desktop');
+function init_Desktop() {
+	if ( rcdObj.view != 'Desktop' ) return;
+	if (!isOPAPub() || isLOPopup()) window.init_page();
+	preload(rcdObj['preload_'+rcdObj.view]);
+}
+function defineFuncs_Desktop() {
+	if ( rcdObj.view != 'Desktop' ) return;
+	try{
+		if (window.initGEV)
+		{
+		 initGEV(0,swipeLeft,swipeRight);
+
+		}
+		} catch(e) { if (window.console) window.console.log(e); }	pageClick = n;
+	pageRClick = n;
+	pageKey = n;
+}
+image26893.rcdData.att_Desktop = 
+{
+	innerHtml:	"<img id=\"tobj26893Img\" src=\"images/Achtergrond%20quiz_Feedback4.jpg\" style=\"position: absolute; border-style: none; left: 0px; top: 0px; width: 1365px; height: 767px;\">",
+	cssText:	"visibility: inherit; position: absolute; left: 0px; top: 0px; width: 1365px; height: 767px; z-index: 0;",
+	cssClasses:	"",
+	htmlId:		"tobj26893",
+	bInsAnc:	0,
+	cwObj:		{
+		"name":	"Achtergrond quiz_Feedback"
+	},
+	objData:	{"a":[0,288,0,[0,0,1365,767]],"borderEffect":{"outline":0,"outlineColor":"#000000","borderWeight":0,"lineStyle":0,"borderColor":"#000000"},"rotateEffect":{"angle":0,"anchorX":50,"anchorY":50},"desktopRect":{"x":0,"y":0,"width":1365,"height":767}}
+};
+text26910.rcdData.att_Desktop = 
+{
+	innerHtml:	"<div name=\"dCon\" style=\"position: absolute; left: 0px; top: 0px; width: 589px; min-height: 674px;\"><div name=\"dCon2\" class=\"ttxt\" style=\"left: 0px; top: 0px; width: 589px; min-height: 674px;\"><p align=\"center\" leglh=\"1.454\" style=\"margin-left:0px;text-indent:0px;line-height:1.454;margin-top:0px;margin-bottom:0px;\"><span style=\"font-size:26pt;\"><span style=\"font-family: &quot;Titillium Web&quot;, sans-serif; color: rgb(0, 40, 87);\">Het </span><strong><span style=\"font-family: &quot;Titillium Web&quot;, sans-serif; color: rgb(0, 40, 87);\">wettelijk verplichte eigen risico</span></strong><span style=\"font-family: &quot;Titillium Web&quot;, sans-serif; color: rgb(0, 40, 87);\"> voor een zorgverzekering is dit jaar </span></span></p>\n\n<p align=\"center\" leglh=\"1.454\" style=\"margin-left:0px;text-indent:0px;line-height:1.454;margin-top:0px;margin-bottom:0px;\"><span style=\"font-size:26pt;\"><strong><span style=\"font-family: &quot;Titillium Web&quot;, sans-serif; color: rgb(0, 40, 87);\">€ 385</span></strong><span style=\"font-family: &quot;Titillium Web&quot;, sans-serif; color: rgb(0, 40, 87);\">. </span><span style=\"font-family: &quot;Titillium Web&quot;, sans-serif; color: rgb(0, 40, 87);\">Het eigen risico is een bedrag dat iemand eerst </span><strong><span style=\"font-family: &quot;Titillium Web&quot;, sans-serif; color: rgb(0, 40, 87);\">uit eigen zak</span></strong><span style=\"font-family: &quot;Titillium Web&quot;, sans-serif; color: rgb(0, 40, 87);\"> moet betalen aan de zorgverzekeraar, voordat hij </span><strong><span style=\"font-family: &quot;Titillium Web&quot;, sans-serif; color: rgb(0, 40, 87);\">recht heeft op vergoeding</span></strong><span style=\"font-family: &quot;Titillium Web&quot;, sans-serif; color: rgb(0, 40, 87);\"> vanuit de basisverzekering</span><span style=\"font-family: &quot;Titillium Web&quot;, sans-serif; color: rgb(0, 40, 87);\">.</span><span style=\"font-family: Helvetica, sans-serif; color: rgb(0, 40, 87);\">&nbsp;</span><span style=\"font-family: Arial, sans-serif; color: rgb(0, 0, 0);\"> </span></span></p></div></div>",
+	cssText:	"visibility: inherit; position: absolute; left: 162px; top: 171px; width: 589px; height: 674px; z-index: 1;",
+	cssClasses:	"",
+	htmlId:		"tobj26910",
+	bInsAnc:	0,
+	cwObj:		{
+		"name":	"Feedback text"
+	},
+	objData:	{"a":[0,32,0,[162,171,589,674]],"borderEffect":{"outline":0,"outlineColor":"#000000","borderWeight":0,"lineStyle":0,"borderColor":"#000000"},"rotateEffect":{"angle":0,"anchorX":50,"anchorY":50},"desktopRect":{"x":162,"y":171,"width":589,"height":674},"dwTextFlags":0,"marginSize":0,"textPublishLang":"Inherit"}
+};
+textbutton27346.rcdData.att_Desktop = 
+{
+	innerHtml:	"<svg viewBox=\"0 0 61 62\" preserveAspectRatio=\"none\" focusable=\"false\" style=\"pointer-events: none; left: 0px; top: 0px; width: 100%; height: 100%; position: absolute;\" aria-hidden=\"true\"><g transform=\"translate(30.5 31)\" style=\"\">\n	<path d=\"M 0 0 L 61 0 L 61 62 L 0 62 L 0 0 Z\" style=\"stroke: rgb(9, 99, 177); stroke-width: 0; stroke-dasharray: none; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 10; fill: rgb(255, 255, 255); fill-opacity:0;filter:alpha(opacity=0); fill-rule: nonzero; opacity:1;filter:alpha(opacity=100); pointer-events: auto;\" transform=\"translate(0 0) translate(-30.5, -31) \" stroke-linecap=\"round\"></path>\n	<pattern id=\"SVGID_26631_6665\" x=\"0\" y=\"0\" width=\"1\" height=\"1\">\n<image x=\"0\" y=\"0\" width=\"61\" height=\"62\" xlink:href=\"images/Verder_roze.png\" preserveAspectRatio=\"none\"></image>\n</pattern>\n<path d=\"M 0 0 L 61 0 L 61 62 L 0 62 L 0 0\" style=\"stroke: rgb(9, 99, 177); stroke-width: 0; stroke-dasharray: none; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 10; fill: url(&quot;#SVGID_26631_6665&quot;); fill-rule: nonzero; opacity:1;filter:alpha(opacity=100); pointer-events: auto;\" transform=\"translate(0 0) translate(-30.5, -31) \" stroke-linecap=\"round\"></path>\n</g>\n	<g transform=\"translate(30.5 31)\">\n		<text font-family=\"Lucida Sans Unicode\" font-size=\"15.9999996\" font-weight=\"normal\" style=\"stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity:1;filter:alpha(opacity=100);\">\n			<tspan x=\"0\" y=\"5.04\" fill=\"#FFFFFF\"></tspan>\n		</text>\n	</g>\n</svg>",
+	cssText:	"visibility: inherit; position: absolute; left: 621px; top: 524px; width: 61px; height: 62px; z-index: 2; cursor: pointer; overflow: visible; pointer-events: none;",
+	cssClasses:	"",
+	htmlId:		"tobj27346",
+	bInsAnc:	1,
+	cwObj:		{
+		"name":	"Button",
+		"arChld":
+	[
+		{type:6,on:2,delay:0,name:'OnMClkGoTo',actItem:function(){ trivExitPage('quiz_merkbeleving_chapter_1_quizvraag_2_slider.html',true,false);
+    if(typeof pF == 'function') pF(); }}
+	]
+	},
+	objData:	{"a":[4,33120,0,[621,524,61,62]],"borderEffect":{"outline":0,"outlineColor":"#000000","borderWeight":0,"lineStyle":0,"borderColor":"#000000"},"rotateEffect":{"angle":0,"anchorX":50,"anchorY":50},"desktopRect":{"x":621,"y":524,"width":61,"height":62},"imgDataNormal":"images/desktop_shape27346.png","imgDataOver":"images/desktop_shape27346_over.png","imgDataDown":"images/desktop_shape27346.png","imgDataDisabled":"images/desktop_shape27346_disabled.png","svgDataNormal":"<g transform=\"translate(30.5 31)\" style=\"\">\n\t<path d=\"M 0 0 L 61 0 L 61 62 L 0 62 L 0 0 Z\" style=\"stroke: rgb(9, 99, 177); stroke-width: 0; stroke-dasharray: none; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 10; fill: rgb(255, 255, 255); fill-opacity: 0; fill-rule: nonzero; opacity: 1; pointer-events: auto;\" transform=\"translate(0 0) translate(-30.5, -31) \" stroke-linecap=\"round\"></path>\n\t<pattern id=\"SVGID_26631_6665\" x=\"0\" y=\"0\" width=\"1\" height=\"1\">\n<image x=\"0\" y=\"0\" width=\"61\" height=\"62\" xlink:href=\"images/Verder_roze.png\" preserveAspectRatio=\"none\"></image>\n</pattern>\n<path d=\"M 0 0 L 61 0 L 61 62 L 0 62 L 0 0\" style=\"stroke: rgb(9, 99, 177); stroke-width: 0; stroke-dasharray: none; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 10; fill: url(&quot;#SVGID_26631_6665&quot;); fill-rule: nonzero; opacity: 1; pointer-events: auto;\" transform=\"translate(0 0) translate(-30.5, -31) \" stroke-linecap=\"round\"></path>\n</g>\n\t<g transform=\"translate(30.5 31)\">\n\t\t<text font-family=\"Lucida Sans Unicode\" font-size=\"15.9999996\" font-weight=\"normal\" style=\"stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;\">\n\t\t\t<tspan x=\"0\" y=\"5.04\" fill=\"#FFFFFF\"></tspan>\n\t\t</text>\n\t</g>\n","svgDataOver":"<g transform=\"translate(30.5 31)\" style=\"\">\n\t<path d=\"M 0 0 L 61 0 L 61 62 L 0 62 L 0 0 Z\" style=\"stroke: rgb(9, 99, 177); stroke-width: 0; stroke-dasharray: none; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 10; fill: rgb(255, 255, 255); fill-opacity: 0; fill-rule: nonzero; opacity: 1; pointer-events: auto;\" transform=\"translate(0 0) translate(-30.5, -31) \" stroke-linecap=\"round\"></path>\n\t<pattern id=\"SVGID_26631_6667\" x=\"0\" y=\"0\" width=\"1\" height=\"1\">\n<image x=\"0\" y=\"0\" width=\"61\" height=\"62\" xlink:href=\"images/Verder_roze_hover.png\" preserveAspectRatio=\"none\"></image>\n</pattern>\n<path d=\"M 0 0 L 61 0 L 61 62 L 0 62 L 0 0\" style=\"stroke: rgb(9, 99, 177); stroke-width: 0; stroke-dasharray: none; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 10; fill: url(&quot;#SVGID_26631_6667&quot;); fill-rule: nonzero; opacity: 1; pointer-events: auto;\" transform=\"translate(0 0) translate(-30.5, -31) \" stroke-linecap=\"round\"></path>\n</g>\n\t<g transform=\"translate(30.5 31)\">\n\t\t<text font-family=\"Lucida Sans Unicode\" font-size=\"15.9999996\" font-weight=\"normal\" style=\"stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;\">\n\t\t\t<tspan x=\"0\" y=\"5.04\" fill=\"#FFFFFF\"></tspan>\n\t\t</text>\n\t</g>\n","svgDataDown":"<g transform=\"translate(30.5 31)\" style=\"\">\n\t<path d=\"M 0 0 L 61 0 L 61 62 L 0 62 L 0 0 Z\" style=\"stroke: rgb(9, 99, 177); stroke-width: 0; stroke-dasharray: none; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 10; fill: rgb(255, 255, 255); fill-opacity: 0; fill-rule: nonzero; opacity: 1; pointer-events: auto;\" transform=\"translate(0 0) translate(-30.5, -31) \" stroke-linecap=\"round\"></path>\n\t<pattern id=\"SVGID_26631_6669\" x=\"0\" y=\"0\" width=\"1\" height=\"1\">\n<image x=\"0\" y=\"0\" width=\"61\" height=\"62\" xlink:href=\"images/Verder_roze.png\" preserveAspectRatio=\"none\"></image>\n</pattern>\n<path d=\"M 0 0 L 61 0 L 61 62 L 0 62 L 0 0\" style=\"stroke: rgb(9, 99, 177); stroke-width: 0; stroke-dasharray: none; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 10; fill: url(&quot;#SVGID_26631_6669&quot;); fill-rule: nonzero; opacity: 1; pointer-events: auto;\" transform=\"translate(0 0) translate(-30.5, -31) \" stroke-linecap=\"round\"></path>\n</g>\n\t<g transform=\"translate(30.5 31)\">\n\t\t<text font-family=\"Lucida Sans Unicode\" font-size=\"15.9999996\" font-weight=\"normal\" style=\"stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;\">\n\t\t\t<tspan x=\"0\" y=\"5.04\" fill=\"#FFFFFF\"></tspan>\n\t\t</text>\n\t</g>\n","svgDataDisabled":"<g transform=\"translate(30.5 31)\" style=\"\">\n\t<path d=\"M 0 0 L 61 0 L 61 62 L 0 62 L 0 0 Z\" style=\"stroke: rgb(9, 99, 177); stroke-width: 0; stroke-dasharray: none; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 10; fill: rgb(255, 255, 255); fill-opacity: 0; fill-rule: nonzero; opacity: 0.6; pointer-events: auto;\" transform=\"translate(0 0) translate(-30.5, -31) \" stroke-linecap=\"round\"></path>\n\t<pattern id=\"SVGID_26631_6671\" x=\"0\" y=\"0\" width=\"1\" height=\"1\">\n<image x=\"0\" y=\"0\" width=\"61\" height=\"62\" xlink:href=\"images/Verder_roze.png\" preserveAspectRatio=\"none\"></image>\n</pattern>\n<path d=\"M 0 0 L 61 0 L 61 62 L 0 62 L 0 0\" style=\"stroke: rgb(9, 99, 177); stroke-width: 0; stroke-dasharray: none; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 10; fill: url(&quot;#SVGID_26631_6671&quot;); fill-rule: nonzero; opacity: 0.6; pointer-events: auto;\" transform=\"translate(0 0) translate(-30.5, -31) \" stroke-linecap=\"round\"></path>\n</g>\n\t<g transform=\"translate(30.5 31)\">\n\t\t<text font-family=\"Lucida Sans Unicode\" font-size=\"15.9999996\" font-weight=\"normal\" style=\"stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 0.6;\">\n\t\t\t<tspan x=\"0\" y=\"5.04\" fill=\"#FFFFFF\"></tspan>\n\t\t</text>\n\t</g>\n","btnState":"enabled","fallbackImg":"<img alt=\"\" title=\"\" style=\"position: absolute; width: 100%; height: 100%; top: 0px; left: 0px;\">"}
+};
+rcdObj.rcdData.att_Desktop = 
+{
+	font:	{"bold":0,"italic":0,"underline":0,"size":"12","color":null,"bgColor":null,"name":"Arial,sans-serif","lineHeight":"1.25","marginTop":"0px","marginBottom":"0px"},
+	pageIdx:	6
+};
+rcdObj.pgWidth_Desktop = pgWidth_desktop;
+rcdObj.preload_Desktop = ["images/Achtergrond%20quiz_Feedback4.jpg","images/Verder_roze.png","images/Verder_roze_hover.png"];
+rcdObj.pgStyle_Desktop = 'position: absolute; left: 0px; top: 0px; width: 1366px; height: 768px; overflow: hidden; visibility: hidden; background-size: auto;'
+rcdObj.backgrd_Desktop = ["#FFFFFF","",0,0,1];
